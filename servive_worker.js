d31ca1a837b040f807041fef60c2d107245ca6da
thdud <thgml1444@naver.com>
2023-10-25 22:42:46 +0900

    service-worker-test 등록

--- servive_worker.js
+++ servive_worker.js
@@ -1,28 +1,3 @@
-const registerServiceWorker = async () => {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register("/servive_worker.js", {
-        scope: "/",
-      });
-      if (registration.installing) {
-        console.log("Service worker installing");
-        alert('Service worker installing')
-      } else if (registration.waiting) {
-        console.log("Service worker waiting");
-        alert('Service worker waiting')
-      } else if (registration.active) {
-        console.log("Service worker active");
-        alert('Service worker active')
-      }
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
-      alert(`Registration failed with ${error}`)
-    }
-  }
-};
-
-registerServiceWorker();
-
 // self.addEventListener("activate", Event => Handle_activate(Event));
 // self.addEventListener("fetch", Event => Handle_fetch(Event));
 // self.addEventListener("install", Event => Handle_install(Event));
