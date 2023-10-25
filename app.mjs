140239bf7cdb43f49bee71fe0d2d9227bd43a29d
thdud <thgml1444@naver.com>
2023-10-25 22:48:42 +0900

    [ TEST ] service-worker-test

--- app.mjs
+++ app.mjs
@@ -1,3 +1,21 @@
+export async function registerServiceWorker(){
+	if ("serviceWorker" in navigator) {
+		try {
+			const registration = await navigator.serviceWorker.register("/servive_worker.js", {
+				scope: "/",
+			});
+			if (registration.installing) {
+				alert('Service worker installin---g')
+			} else if (registration.waiting) {
+				alert('Service worker ---waiting')
+			} else if (registration.active) {
+				alert('Service worker act--ive')
+			}
+		} catch (error) {
+			alert(`Registration failed with ${error}`)
+		}
+	}
+}
 
 export async function Start(){
   ShowContent('welcome');
