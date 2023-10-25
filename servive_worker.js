300efce41bf1d7319ae96108fbd9f132427145c4
thdud <thgml1444@naver.com>
2023-10-25 22:15:17 +0900

    [ ADD ] service-worker를 등록하라. - PWA에서 service worker는 JS작업 외의 오프라인 작업을 할 수 있게 해 주는 API이다.

---
+++ servive_worker.js
@@ -0,0 +1,40 @@
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("Service worker installing");
+        alert('Service worker installing')
+      } else if (registration.waiting) {
+        console.log("Service worker waiting");
+        alert('Service worker waiting')
+      } else if (registration.active) {
+        console.log("Service worker active");
+        alert('Service worker active')
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+      alert(`Registration failed with ${error}`)
+    }
+  }
+};
+
+registerServiceWorker();
+
+// self.addEventListener("activate", Event => Handle_activate(Event));
+// self.addEventListener("fetch", Event => Handle_fetch(Event));
+// self.addEventListener("install", Event => Handle_install(Event));
+
+// function Handle_activate(Event){
+//  console.log(arguments.callee.toString().match(/function ([^\(]+)/)[1] + ' ' + Event);
+// }
+
+// function Handle_fetch(Event){
+//  console.log(arguments.callee.toString().match(/function ([^\(]+)/)[1] + ' ' + Event);
+// }
+
+// function Handle_install(Event){
+//  console.log(arguments.callee.toString().match(/function ([^\(]+)/)[1] + ' ' + Event);
+// }
