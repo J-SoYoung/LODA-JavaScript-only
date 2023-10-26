debda9e98600fff396abe39671234e0e3b796f84
thdud <thgml1444@naver.com>
2023-10-26 10:49:33 +0900

    SPA를 위한 window match함수 test

--- app.mjs
+++ app.mjs
@@ -21,7 +21,10 @@ registerServiceWorker();
 
 // window onload 후 함수 실행
 export async function Start(){
-  ShowContent('welcome');
+	// check if not PWA
+	if (window.matchMedia('display-mode: standalone').matches == false)
+	alert('PWA를 설치하세요', window.matchMedia('display-mode: standalone').matches)
+  // ShowContent('welcome');
 }
 
 async function ShowContent(ContentName){
